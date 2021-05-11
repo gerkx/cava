a98388ff16da403570b2b19586c8781adb176a5f
Patrick Gerke <ptgerke@gmail.com>
2021-05-12 00:26:22 +0200

    initial commit

---
+++ Components/storybookUtils/StoryWrapper.svelte
@@ -0,0 +1,32 @@
+<div>
+    <slot />
+</div>
+
+<script lang="ts">
+    import { onMount } from 'svelte';
+    import starlette from 'starlette';
+
+    onMount(() => {
+        // starlette.init();
+        starlette.initAs('AEFT', 'gradient', 0);
+        // console.log('harp')
+        console.log(window.getComputedStyle(document.documentElement)
+            .getPropertyValue('--tabs-active')
+        )
+    })
+</script>
+
+<style lang="scss">
+    :root {
+    font-size:16px;
+    box-sizing: border-box;
+    --color-bg: rgb(35,35,35)
+  }  
+
+  div {
+    background-color: var(--color-bg);
+    padding: 5rem;
+    height: 150px;
+    font-size: 25px;
+  }
+</style>
